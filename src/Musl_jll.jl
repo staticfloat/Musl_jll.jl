81b5b8086a4216ca5d2b6f0ea588069d7db00162
Elliot Saba <staticfloat@gmail.com>
2025-03-03 09:08:30 +0000

    Musl v1.1.25

--- src/Musl_jll.jl
+++ src/Musl_jll.jl
@@ -1,9 +1,5 @@
-# Use baremodule to shave off a few KB from the serialized `.ji` file
-baremodule Musl_jll
-using Base
-using Base: UUID
-import JLLWrappers
+module Musl_jll
+using LazyJLLWrappers
+@generate_jll_from_toml()
+end # module Musl_jll
 
-JLLWrappers.@generate_main_file_header("Musl")
-JLLWrappers.@generate_main_file("Musl", UUID("3490aa6f-05e0-5d3a-9648-5b0c647c155f"))
-end  # module Musl_jll
